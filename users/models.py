4ea8d51a25f76bd498f84235c1edd5ce63b5d175
exceedcat <kattdaria@gmail.com>
2017-12-04 16:28:29 +0200

    Put models in users and repositories apps

--- users/models.py
+++ users/models.py
@@ -1,6 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Permission
+from repositories.models import Repository
 
 
-# class User(AbstractUser):
-#     pass
+class User(AbstractUser):
+    avatar = models.CharField(max_length=4096, blank=True)
+    repositories = models.ManyToManyField('Repository', related_name='users', through='UserPermissions')
+
+
+class UserPermissions(Permission):
+    user = models.ForeignKey('User')
+    repository = models.ForeignKey(Repository)
+
