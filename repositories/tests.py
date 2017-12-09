947e75a63f1c614837be647e4fc4e6a584fd0f8a
Olena Holub <else.golub@gmail.com>
2017-12-09 23:14:14 +0200

    Change comments for methods

--- repositories/tests.py
+++ repositories/tests.py
@@ -14,7 +14,7 @@ class TestRepositoryApi(APITestCase):
 
     def test_create_repository(self):
         """
-        Ensure we can create a new repository object.
+        Test case for create a repository
         """
         url = reverse('repositories:repository-list')
         data = {'name': 'Project1'}
@@ -25,7 +25,7 @@ class TestRepositoryApi(APITestCase):
 
     def test_list_repositories(self):
         """
-        Ensure we can list all repositories.
+        Test case for get all repository
         """
         url = reverse('repositories:repository-list')
         response = self.client.get(url, format='json')
@@ -36,7 +36,7 @@ class TestRepositoryApi(APITestCase):
 
     def test_get_repository(self):
         """
-        Ensure we can get a specific repository object.
+        Test case for get a repository
         """
         url = reverse('repositories:repository-detail', kwargs={'pk': 1})
         response = self.client.get(url, format='json')
@@ -47,7 +47,7 @@ class TestRepositoryApi(APITestCase):
 
     def test_put_repository(self):
         """
-        Ensure we can update a specific repository object.
+        Test case for update a repository
         """
         url = reverse('repositories:repository-detail', kwargs={'pk': 1})
         data = {'name': 'Project2'}
@@ -59,9 +59,10 @@ class TestRepositoryApi(APITestCase):
 
     def test_delete_repository(self):
         """
-        Ensure we can delete a specific repository object.
+        Test case for delete a repository
         """
         url = reverse('repositories:repository-detail', kwargs={'pk': 1})
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Repository.objects.count(), 0)
+
