8da6a032a2e0c2a3171a4bcabe2ce6419b0938fc
Max Krivich <maxkrivich@gmail.com>
2017-12-14 21:45:44 +0200

    [WIP] start refactoring users crud

--- users/serializers.py
+++ users/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from users.models import User
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
--- users/views.py
+++ users/views.py
@@ -1,18 +1,60 @@
-from users.models import User
-from rest_framework import generics
+from django.http import Http404
+from rest_framework import status
+from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
+from rest_framework.views import APIView
 
+from users.models import User
 from users.serializers import UserSerializer
 
 
-class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserList(APIView):
+    def get(self, request, *args, **kwargs):
+        users = User.objects.all()
+        serializer_context = {
+            'request': Request(request),
+        }
+        serializer = UserSerializer(users, context=serializer_context, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer_context = {
+            'request': Request(request),
+        }
+        serializer = UserSerializer(data=request.data, context=serializer_context)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = UserSerializer
+class UserDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
 
-    def get_queryset(self):
-        return User.objects.all().filter(username=self.request.user)
+    def get(self, request, pk, *args, **kwargs):
+        user = self.get_object(pk)
+        serializer_context = {
+            'request': Request(request),
+        }
+        serializer = UserSerializer(user, context=serializer_context)
+        return Response(serializer.data)
+
+    def put(self, request, pk, *args, **kwargs):
+        user = self.get_object(pk)
+        serializer_context = {
+            'request': Request(request),
+        }
+        serializer = UserSerializer(user, data=request.data, context=serializer_context)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, *args, **kwargs):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
