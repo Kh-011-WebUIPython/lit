7ae23b5f12ac13c40fdaaf5c6d107447558b32d0
Max Krivich <maxkrivich@gmail.com>
2017-12-23 22:34:46 +0200

    Add support null avatar

--- users/serializers.py
+++ users/serializers.py
@@ -6,9 +6,26 @@ from users.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    avatar = serializers.ImageField(use_url=True)
+    avatar = serializers.ImageField(use_url=True, required=False)
     url = ParameterisedHyperlinkedIdentityField(view_name='users:user-detail', lookup_fields=(('pk', 'user_id'),),
                                                 read_only=True)
+
+    def create(self, validated_data):
+        user = User(username=validated_data.get('username', None),
+                    email=validated_data.get('email', None),
+                    avatar=validated_data.get('avatar', None))
+        user.set_password(validated_data.get('password', None))
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        for field in validated_data:
+            if field == 'password':
+                instance.set_password(validated_data.get(field))
+            else:
+                instance.__setattr__(field, validated_data.get(field))
+        instance.save()
+        return instance
 
     class Meta:
         model = User
