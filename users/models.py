52b007b119518ecd64313e2c080f59831e50c016
Max Krivich <maxkrivich@gmail.com>
2017-12-26 20:06:11 +0200

    Add description for users app

--- users/models.py
+++ users/models.py
@@ -5,13 +5,23 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-def get_file_path(instance, filename):
+def get_file_path(instance, filename) -> str:
+    """
+    Method for generating path for user avatar on local storage
+
+    :param instance: User object
+    :param filename: name of uploaded file
+    :return: server path relatively defaults storage [lit.settings.base.MEDIA_*]
+    """
     ext = filename.split('.')[-1]
     filename = "%s.%s" % (uuid.uuid4(), ext)
     return os.path.join('users/avatars', filename)
 
 
 class User(AbstractUser):
+    """
+    Base user model depends on default django abstract user model
+    """
     avatar = models.ImageField(upload_to=get_file_path,
                                null=True,
                                blank=True)
