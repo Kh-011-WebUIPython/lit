551f9f2cc08322a9d1cef656c7b7d062cbd4f7bd
Max Krivich <maxkrivich@gmail.com>
2017-12-26 19:32:07 +0200

    Add description for repository app

--- repositories/serializers.py
+++ repositories/serializers.py
@@ -11,12 +11,21 @@ logger = logging.getLogger(__name__)
 
 
 class RepositorySerializer(serializers.ModelSerializer):
+    """
+    This class serialize main repository fields(url, id, users, name, description, default branch, etc)
+    """
     users = UserSerializer(many=True, read_only=True)
     url = ParameterisedHyperlinkedIdentityField(view_name='repositories:repository-detail',
                                                 lookup_fields=(('pk', 'repository_id'),),
                                                 read_only=True)
 
-    def create(self, validated_data):
+    def create(self, validated_data: dict) -> Repository:
+        """
+        Overrated method for implement own permission logic on repository create
+
+        :param validated_data: fields from http request for creating repository
+        :return: reference on new repository instance
+        """
         repository = Repository(name=validated_data.get('name', None),
                                 description=validated_data.get('description', None))
         repository.save()
@@ -26,7 +35,14 @@ class RepositorySerializer(serializers.ModelSerializer):
         permission.save()
         return repository
 
-    def update(self, instance, validated_data):
+    def update(self, instance: Repository, validated_data: dict) -> Repository:
+        """
+         Overrated method for implement own logic on repository update
+
+        :param instance: repository object
+        :param validated_data: updated fields from http request
+        :return: updated repository object
+        """
         for field in validated_data:
             instance.__setattr__(field, validated_data.get(field))
         instance.save()
@@ -34,4 +50,4 @@ class RepositorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Repository
-        fields = ('url', 'id', 'name', 'description', 'users', )
+        fields = ('url', 'id', 'name', 'description', 'users',)
--- repositories/views.py
+++ repositories/views.py
@@ -16,6 +16,9 @@ logger = logging.getLogger(__name__)
 
 
 class RepositoryList(ListCreateAPIView):
+    """
+    View for handle GET and POST methods on repository endpoint
+    """
     queryset = Repository.objects.all()
     serializer_class = RepositorySerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
@@ -24,35 +27,64 @@ class RepositoryList(ListCreateAPIView):
 
 
 class RepositoryDetail(APIView):
+    """
+    View for handle main http methods(GET,PUT,DELETE) on repository entity
+    URL --> /repositories/(?P<repository_id>[0-9]+)$
+    """
     permission_classes = (IsOwnerOrReadOnly,)
 
-    def get_object(self, rpk):
+    @staticmethod
+    def get_object(repository_id: int) -> Repository:
+        """
+        Find repository by repository ID in DB
+
+        :param repository_id: parameter from this url--> /repositories/(?P<repository_id>[0-9]+)$
+        :return: Repository or rise exception HTTP_404
+        """
         try:
-            return Repository.objects.get(pk=rpk)
+            return Repository.objects.get(pk=repository_id)
         except Repository.DoesNotExist as e:
             logger.exception(e)
             raise Http404
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        """
+        Method for handle GET request on /repositories/{repository_id}
+
+        :param request: incoming http request
+        :param args: other parameters
+        :param kwargs: dict parsed url variables {"repository_id": "id"}
+        :return: JSON data with specific repository
+        """
         repo = self.get_object(kwargs['repository_id'])
-        serializer_context = {
-            'request': Request(request),
-        }
-        serializer = RepositorySerializer(repo, context=serializer_context)
+        serializer = RepositorySerializer(repo, context={'request': Request(request)})
         return Response(serializer.data)
 
-    def put(self, request, *args, **kwargs):
+    def put(self, request: Request, *args, **kwargs) -> Response:
+        """
+        Method for handle HTTP PUT request on /repositories/{repository_id}
+
+        :param request: incoming http request
+        :param args: other parameters
+        :param kwargs: dict parsed url variables {"repository_id": "id"}
+        :return: JSON data with specific repository, else return 400 and reason why that's happen
+        """
         repo = self.get_object(kwargs['repository_id'])
-        serializer_context = {
-            'request': Request(request),
-        }
-        serializer = RepositorySerializer(repo, data=request.data, context=serializer_context)
+        serializer = RepositorySerializer(repo, data=request.data, context={'request': Request(request)})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, *args, **kwargs):
+    def delete(self, request: Request, *args, **kwargs) -> Response:
+        """
+        Method for handle HTTP DELETE request on /repositories/{repository_id}
+
+        :param request: incoming http request
+        :param args: other parameters
+        :param kwargs: dict parsed url variables {"repository_id": "id"}
+        :return: On success HTTP 204 status code, else return 404
+        """
         repo = self.get_object(kwargs['repository_id'])
         repo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
