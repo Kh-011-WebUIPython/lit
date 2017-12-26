d36b2271316da528589017048ec315ff8287b8eb
Max Krivich <maxkrivich@gmail.com>
2017-12-26 17:56:50 +0200

    Merge errors with repo delete

--- repositories/views.py
+++ repositories/views.py
@@ -67,3 +67,5 @@ class RepositoryDetail(APIView):
 
     def delete(self, request, *args, **kwargs):
         repo = self.get_object(kwargs['repository_id'])
+        repo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
