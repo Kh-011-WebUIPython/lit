13348a59c1f297694c5fa967c6da5da7a1ebcdc9
volodindmitriy121 <volodindmitriy121@gmail.com>
2018-01-24 21:06:18 +0200

    Rewriting push command

--- repositories/urls.py
+++ repositories/urls.py
@@ -6,7 +6,10 @@ from repositories import views
 urlpatterns = [
     url(r'^repositories/$', views.RepositoryList.as_view(), name='repository-list'),
     url(r'^repositories/(?P<repository_id>[0-9]+)$', views.RepositoryDetail.as_view(), name='repository-detail'),
-    url(r'^repositories/(?P<repository_id>[0-9]+)/push/$', views.push, name='repository-push')
+    url(r'^repositories/(?P<repository_id>[0-9]+)/push_check_commits/$', views.push_check_commits, name='repository-push-check-commits'),
+    url(r'^repositories/(?P<repository_id>[0-9]+)/push_add_commits/$', views.push_add_commits,
+        name='repository-push-add-commits')
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- repositories/views.py
+++ repositories/views.py
@@ -2,19 +2,24 @@ import base64
 import json
 import logging
 import os
+import secrets
 import struct
 
 from django.http import Http404
 from rest_framework import status, filters
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from branches.models import Branch
+from commits.models import Commit
+from lit.permissions import IsContributorOrReadOnly
 from lit.permissions import IsOwnerOrReadOnly
 from repositories.models import Repository
+from users.models import User
 from repositories.serializers import RepositorySerializer
 
 logger = logging.getLogger(__name__)
@@ -100,7 +105,8 @@ class RepositoryDetail(APIView):
 
 
 @api_view(['POST'])
-def push(request: Request, *args, **kwargs) -> Response:
+@permission_classes((IsContributorOrReadOnly, IsOwnerOrReadOnly))
+def push_check_commits(request: Request, *args, **kwargs) -> Response:
     """
     Method for handle POST request on /repositories/{repository_id}/push
     :param request: incoming http request
@@ -108,11 +114,37 @@ def push(request: Request, *args, **kwargs) -> Response:
     :param kwargs: dict parsed url variables {"repository_id": "id"}
     :return: on success HTTP 200 status code, else 404
     """
+    # TODO check for permissions
+    # Phase 1
+
+    json_content = json.loads(request.body)
+    try:
+        repo = Repository.objects.get(pk=kwargs['repository_id'])
+    except Repository.DoesNotExist:
+        raise Http404
+
+    comment_diff = []
+    if (json_content['branch_name'] in Branch.objects.all()):
+        for commit_hash in json_content['commits_hashes']:
+            if (commit_hash not in Commit.objects.all()):
+                comment_diff.append(commit_hash)
+    else:
+        branch = Branch(name=json_content['branch_name'], repository=repo)
+        branch.save()
+        comment_diff = json_content['commits_hash'][:]
+
+    response = {"session_token": secrets.token_hex(16), "commits": comment_diff}
+    return Response(response, status=status.HTTP_200_OK)
+
+# Phase 2
+@api_view(['POST'])
+@permission_classes((IsContributorOrReadOnly, IsOwnerOrReadOnly))
+def push_add_commits(request: Request, *args, **kwargs) -> Response:
     decoded_request = base64.decodebytes(request)
     curr_size = 8
     size_of_package = struct.unpack('Q', decoded_request[:8])[0]
 
-    package_content = json.loads(decoded_request[curr_size:size_of_package+8].decode('utf-8'))
+    package_content = json.loads(decoded_request[curr_size:size_of_package + 8].decode('utf-8'))
     curr_size = size_of_package
     log_size = int(package_content['logs'])
 
@@ -122,15 +154,25 @@ def push(request: Request, *args, **kwargs) -> Response:
     os.chdir(repo_path)
 
     with open('commits_log.json', 'w') as commit_log:
-        commit_log.write( decoded_request[curr_size:curr_size + log_size].decode('utf-8'))
+        commit_log.write(decoded_request[curr_size:curr_size + log_size].decode('utf-8'))
+        commits_log_dict = json.loads(commit_log)
         commit_log.close()
+
+    for commit in commits_log_dict:
+        commit_to_save = Commit(user=User.objects.filter(username=commit['user']),
+                        long_hash=commit['long_hash'],
+                        branch= Branch.objects.filter(name=commit['branch_name']),
+                        commit_time=commit['datetime'],
+                        comment=commit['message'])
+        commit_to_save.save()
 
     curr_size += log_size
 
     for commit in package_content['commits']:
-        with open(list(commit.keys())[0] + '.zip', 'wb') as commit_zip:
-            commit_zip.write(decoded_request[curr_size:curr_size + int(commit[list(commit.keys())[0]])])
-            curr_size += int(commit[list(commit.keys())[0]])
+        commit_key = list(commit.keys())[0]
+        with open(commit_key + '.zip', 'wb') as commit_zip:
+            commit_zip.write(decoded_request[curr_size:curr_size + int(commit[commit_key])])
+            curr_size += int(commit[commit_key])
             commit_zip.close()
 
     os.chdir(curr_path)
